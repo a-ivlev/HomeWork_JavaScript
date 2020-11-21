967c4f54dc508a70ad35054748dfbd12dadc9f9a
origin <a.y.ivlev@gmail.com>
2020-11-21 15:17:01 +0300

    Home work 1

---
+++ homework-1/Task_2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Объявить две переменные: admin и name.
+let admin, name;
+// Записать в name строку "Василий".
+name = 'Василий';
+// Скопировать значение из name в admin.
+admin = name;
+// Вывести в консоль переменную admin (должно вывести "Василий").
+console.log(admin);
